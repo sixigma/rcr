6e7e5ff3bcb59f5e067a61497ec539a743644142
T. K <kimtj0@gmail.com>
2021-01-07 19:59:44 +0900

    Add and render headers and source files regarding maps + edit gameScene and others

--- map1.cpp
+++ map1.cpp
@@ -4,28 +4,88 @@
 
 HRESULT map1::init()
 {
-	setRoomIdx(1);
-	if (getPrevRoomIdx() == 2) pl->getPos().x = 3025;
-	gameScene::setViewport(pl->getPos().x, pl->getPos().y);
+	setMapNum(1);
+	currPlPos = &pl->getPos();
+	if (getPrevMapNum() == 0) *currPlPos = { 154, 568 };
+	else if (getPrevMapNum() == 2) *currPlPos = { _totRegion.right - 48, 568 };
+	else *currPlPos = { 154, 568 };
+
+	obst.push_back({ 0, 300, 92, 480 });
+	obst.push_back({ 92, 292, 160, 484 });
+	obst.push_back({ 352, 296, 416, 484 });
+	obst.push_back({ 416, 292, 484, 484 });
+	obst.push_back({ 484, 300, 3036, 480 });
+	unlandable = { 160, 0, 352, 480 };
+
+	prevPlPos = *currPlPos;
+	gameScene::setViewport(currPlPos->x, currPlPos->y);
+
 	return S_OK;
 }
 
 void map1::release()
 {
+	obst.clear();
 }
 
 void map1::update()
 {
-	pl->update();
+	if (currPlPos->x + 32 > 3026) gameScene::goToMap(2);
 
-	if (pl->getPos().x > 3026)
-		gameScene::goToRoom(2);
+	if (currPlPos->x < 32) currPlPos->x = 32;
 
+	if (currPlPos->y - 4 < unlandable.bottom && prevPlPos.y - 4 >= unlandable.bottom &&
+	currPlPos->x + 32 >= unlandable.left && currPlPos->x - 32 <= unlandable.right)
+		currPlPos->y = unlandable.bottom + 4;
 
-	gameScene::updateViewport(pl->getPos().x, pl->getPos().y);
+	for (size_t i = 0; i < obst.size(); ++i)
+	{
+		if (currPlPos->y < obst[i].top - 1 && prevPlPos.y > obst[i].top - 1 &&
+			currPlPos->x + 32 >= obst[i].left && currPlPos->x - 32 <= obst[i].right)
+			currPlPos->y = obst[i].top - 1;
+		else if (currPlPos->y - 4 < obst[i].bottom && prevPlPos.y - 4 >= obst[i].bottom &&
+			currPlPos->x + 32 > obst[i].left && currPlPos->x - 32 < obst[i].right)
+			currPlPos->y = obst[i].bottom + 4;
+		if (currPlPos->x - 32 > obst[i].left && currPlPos->x - 32 < obst[i].right
+			&& currPlPos->y - 4 < obst[i].bottom && prevPlPos.x != currPlPos->x)
+			currPlPos->x = obst[i].right + 32;
+		else if (currPlPos->x + 32 > obst[i].left && currPlPos->x + 32 < obst[i].right
+				 && currPlPos->y - 4 < obst[i].bottom && prevPlPos.x != currPlPos->x)
+			currPlPos->x = obst[i].left - 32;
+	}
+
+	if (currPlPos->y > 96 + 640) currPlPos->y = 96 + 640;
+
+	gameScene::updateViewport(currPlPos->x, currPlPos->y);
+	prevPlPos = *currPlPos;
 }
 
 void map1::render()
 {
-	IMG->find("ÀüÅõ ¸Ê 1")->render(getMemDC(), _currOrg.x, 96, _currOrg.x, 0, WINW, 640);
+	IMG->find("¸Ê 1")->render(getMemDC(), _currOrg.x, 96, _currOrg.x, 0, WINW, 640);
+
+#ifdef _DEBUG
+	{
+		if (KEY->isToggledOn(VK_CAPITAL))
+		{
+			HPEN hPen = CreatePen(PS_SOLID, 1, RGB(0, 255, 0));
+			HBRUSH hBrush = CreateSolidBrush(RGB(0, 255, 0));
+			HPEN hOPen = (HPEN)SelectObject(getMemDC(), hPen);
+			HBRUSH hOBrush = (HBRUSH)SelectObject(getMemDC(), hBrush);
+			for (size_t i = 0; i < obst.size(); ++i)
+			{
+				DrawRct(getMemDC(), obst[i]);
+			}
+			DeleteObject(SelectObject(getMemDC(), hOPen));
+			DeleteObject(SelectObject(getMemDC(), hOBrush));
+			hPen = CreatePen(PS_SOLID, 1, RGB(200, 50, 0));
+			hBrush = CreateSolidBrush(RGB(200, 50, 0));
+			hOPen = (HPEN)SelectObject(getMemDC(), hPen);
+			hOBrush = (HBRUSH)SelectObject(getMemDC(), hBrush);
+			DrawRct(getMemDC(), unlandable);
+			DeleteObject(SelectObject(getMemDC(), hOPen));
+			DeleteObject(SelectObject(getMemDC(), hOBrush));
+		}
+	}
+#endif
 }
---
+++ map3.cpp
@@ -0,0 +1,100 @@
+#include "stdafx.h"
+#include "map3.h"
+#include "player.h"
+
+HRESULT map3::init()
+{
+	setMapNum(3);
+	currPlPos = &pl->getPos();
+	
+	if (getPrevMapNum() == 4) *currPlPos = { _totRegion.right - 40, 592 };
+	else if (getPrevMapNum() == 301) *currPlPos = { 338, 484 + 8 };
+	else if (getPrevMapNum() == 302) *currPlPos = { 734, 484 + 8 };
+	else if (getPrevMapNum() == 303) *currPlPos = { 1144, 484 + 8 };
+	else if (getPrevMapNum() == 304) *currPlPos = { 1824, 484 + 8 };
+	else *currPlPos = { _totRegion.left + 40, 592 };
+
+	obst.push_back({ 0, 96, 416, 480 });
+	obst.push_back({ 416, 96, 480, 484 });
+	obst.push_back({ 480, 96, 1950, 480 });
+	obst.push_back({ 1950, 96, _totRegion.right, 484 });
+
+	prevPlPos = *currPlPos;
+	gameScene::setViewport(pl->getPos().x, pl->getPos().y);
+
+	return S_OK;
+}
+
+void map3::release()
+{
+	obst.clear();
+}
+
+void map3::update()
+{
+	if (currPlPos->x < 32) gameScene::goToMap(2);
+	else if (currPlPos->x + 32 > _totRegion.right) gameScene::goToMap(4);
+	else if (currPlPos->y <= 484 && KEY->press('W'))
+	{
+		if (currPlPos->x - 32 <= 288 && currPlPos->x + 32 >= 388)
+		{
+			// goToMap(301); // 1번 상점으로
+		}
+		else if (currPlPos->x - 32 <= 644 && currPlPos->x + 32 >= 824)
+		{
+			// goToMap(302); // 2번 상점으로
+		}
+		else if (currPlPos->x - 32 <= 1068 && currPlPos->x + 32 >= 1220)
+		{
+			// goToMap(303); // 3번 상점으로
+		}
+		else if (currPlPos->x - 32 <= 1732 && currPlPos->x + 32 >= 1916)
+		{
+			// goToMap(304); // 4번 상점으로
+		}
+	}
+
+	for (size_t i = 0; i < obst.size(); ++i)
+	{
+		if (currPlPos->y < obst[i].top - 1 && prevPlPos.y > obst[i].top - 1 &&
+			currPlPos->x + 32 >= obst[i].left && currPlPos->x - 32 <= obst[i].right)
+			currPlPos->y = obst[i].top - 1;
+		else if (currPlPos->y - 4 < obst[i].bottom && prevPlPos.y - 4 >= obst[i].bottom &&
+			currPlPos->x + 32 > obst[i].left && currPlPos->x - 32 < obst[i].right)
+			currPlPos->y = obst[i].bottom + 4;
+		if (currPlPos->x - 32 > obst[i].left && currPlPos->x - 32 < obst[i].right
+			&& currPlPos->y - 4 < obst[i].bottom && prevPlPos.x != currPlPos->x)
+			currPlPos->x = obst[i].right + 32;
+		else if (currPlPos->x + 32 > obst[i].left && currPlPos->x + 32 < obst[i].right
+				 && currPlPos->y - 4 < obst[i].bottom && prevPlPos.x != currPlPos->x)
+			currPlPos->x = obst[i].left - 32;
+	}
+
+	if (currPlPos->y > 96 + 640) currPlPos->y = 96 + 640;
+
+	gameScene::updateViewport(currPlPos->x, currPlPos->y);
+	prevPlPos = *currPlPos;
+}
+
+void map3::render()
+{
+	IMG->find("맵 3")->render(getMemDC(), _currOrg.x, 96, _currOrg.x, 0, WINW, 640);
+
+#ifdef _DEBUG
+	{
+		if (KEY->isToggledOn(VK_CAPITAL))
+		{
+			HPEN hPen = CreatePen(PS_SOLID, 1, RGB(0, 255, 0));
+			HBRUSH hBrush = CreateSolidBrush(RGB(0, 255, 0));
+			HPEN hOPen = (HPEN)SelectObject(getMemDC(), hPen);
+			HBRUSH hOBrush = (HBRUSH)SelectObject(getMemDC(), hBrush);
+			for (size_t i = 0; i < obst.size(); ++i)
+			{
+				DrawRct(getMemDC(), obst[i]);
+			}
+			DeleteObject(SelectObject(getMemDC(), hOPen));
+			DeleteObject(SelectObject(getMemDC(), hOBrush));
+		}
+	}
+#endif
+}
