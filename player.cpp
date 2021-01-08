0cd3b3d4ad3b87ca9fc8173fcbc8d58e8a3d2dbd
rttswy <rttswy@nate.com>
2021-01-08 18:28:46 +0900

    +++추가
    winAPI.cpp
    13번 째 줄  BOOL  _moveKeyDisabled, _isInShop, _shouldBePaused;
    
    stdafx.h
    44번 째 줄  extern BOOL _moveKeyDisabled, _isInShop, _shouldBePaused;
    
    character.h
    3~52번 째줄 플레이어 상태, 렉트 및 이미지, 능력치, bool값
    
    player.h
    6~9번 째 줄 상속받은 구조체 변수 정의
    
    player.cpp
    10~20번 째줄  상태 및 능력치 초기화
    30번 줄   if (!_moveKeyDisabled && !_isInShop) 조건문 추가
    
    ---------------------------------------------------------------------------------
    
    ---제거:
    
    playground.cpp
    
    24~25줄  l = new LineManager;
      l->CreateLine(MakeRct(300, 800, 200, 200), 20, true, 2, "가na다ra마b사 아");
    
    40번 째 줄   l->update();
    50번 째 줄 l->update();

--- player.cpp
+++ player.cpp
@@ -1,10 +1,24 @@
 #include "stdafx.h"
 #include "player.h"
 
+
 HRESULT player::init()
 {
 	pos = { 10, 400 };
 	return S_OK;
+
+	_p_state = IDLE;
+
+	_p_status.agility = 15;
+	_p_status.endure = 15;
+	_p_status.energy = 15;
+	_p_status.guard = 15;
+	_p_status.hp = 63;
+	_p_status.kick = 15;
+	_p_status.power = 15;
+	_p_status.punch = 15;
+	_p_status.weapon = 15;
+
 }
 
 void player::release()
@@ -13,14 +27,27 @@ void player::release()
 
 void player::update()
 {
-	if (KEY->press('D'))
-		pos.x += 12;
-	else if (KEY->press('A'))
-		pos.x -= 12;
-	if (KEY->press('W'))
-		pos.y -= 12;
-	else if (KEY->press('S'))
-		pos.y += 12;
+	if (!_moveKeyDisabled && !_isInShop)
+	{
+		if (KEY->press('D'))
+			pos.x += 12;
+		else if (KEY->press('A'))
+			pos.x -= 12;
+		if (KEY->press('W'))
+			pos.y -= 12;
+		else if (KEY->press('S'))
+			pos.y += 12;
+		/*
+		if(KEY->press('j'))
+			//발차기 공격 & 줍기(&던지기)
+		if(KEY->press('k'))
+			//주먹 공격 & 줍기(&던지기)
+		if(KEY->press('k') && KEY->press('j'))
+			//점프
+
+			//대쉬후 점프는 더높이 1번 맵에서 점프후 착지가 가능한 정도의 높이
+		*/
+	}
 }
 
 void player::render()
