bc3a290c5bf89d6e50ed3a817cebfc225f749f4e
bbanttan <jjara82@naver.com>
2021-01-09 21:48:08 +0900

    트리거, 폰트, 대사 txt 파일 추가

--- LineManager.h
+++ LineManager.h
@@ -1,22 +1,44 @@
+
 #pragma once
 #include "singleton.h"
 #include "Lines.h"
+#include <fstream>
 #include <vector>
+
+enum Trigger
+{
+	NO,			//그냥 초기화
+	P_DIE,		//플레이어가 죽었을때
+	E_DIE,		//적이 죽었을때
+	E_HELP,		//적이 도망
+	MAP1,		//맵1 일때
+	MAP2,		//맵2 일때
+	MAP3,		//맵3 일때
+	MAP4,		//맵4 일때
+	MAP5		//맵5 일떄
+};
 
 class LineManager : public singleton<LineManager>
 {
 private:
+	bool POSITION;
 	vector<Lines*> _vLine;
 
 public:
+	LineManager();
+	~LineManager();
+
 	virtual HRESULT init();
 	virtual void release();
 	virtual void update();
 	virtual void render();
 
 	void CreateLine(POINT _rect, int _fontsize, string _Lines, int _time = 99999);
+	void trgLine(Trigger _trg, string _name = "no");
 
 	void AllDeleteLine();
+
+	string txtFind(string _line, string _name = "no", int _plusLine = 1);
 
 };
 
