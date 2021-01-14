724b55a86ae257ba2582685500b10fe8c17ac5ea
garytto <76801853+garytto@users.noreply.github.com>
2021-01-14 16:34:48 +0900

    item getter setter

--- item.cpp
+++ item.cpp
@@ -2,18 +2,15 @@
 #include "item.h"
 #include "character.h"
 
-HRESULT item::init()
+HRESULT item::init(string _name)
 {
-	for (int i = 0; i < IMAX; i++)
-	{
-		_item[i]._inventory = false;
-	}
-	itemlist();
+	itemlist(_name);
 	return S_OK;
 }
 
 void item::release()
 {
+
 }
 
 void item::update()
@@ -23,179 +20,214 @@ void item::update()
 
 void item::render()
 {
+
 }
 
-void item::itemlist()
+void item::itemlist(string _name)
 {
 	//ÈÞ´ëºÒ°¡´É
 	//shop1 Ä«Æä
-	_item[0].name = "Coffe";
-	_item[0]._plusenergy = 2;
-	_item[0]._recoveryhp = 16;
-	_item[0]._plusmaxHP = 4;
-	_item[0]._price2 = 95;
-
-	_item[1].name = "Tea";
-	_item[1]._plusenergy = 4;
-	_item[1]._price2 = 95;
-
-	_item[2].name = "Hot Cocoa";
-	_item[2]._plusguard = 1;
-	_item[2]._recoveryhp = 24;
-	_item[2]._plusmaxHP = 6;
-	_item[2]._price1 = 1;
-	_item[2]._price2 = 25;
-
-	_item[3].name = "Pancakes";
-	_item[3]._recoveryhp = 32;
-	_item[3]._plusmaxHP = 8;
-	_item[3]._price1 = 3;
-	_item[3]._price2 = 30;
-
-	_item[4].name = "Waffles";
-	_item[4]._plusagility = 1;
-	_item[4]._plusendure = 1;
-	_item[4]._recoveryhp = 32;
-	_item[4]._plusmaxHP = 8;
-	_item[4]._price1 = 4;
-	_item[4]._price2 = 10;
-
-	//shop3 ½º½Ã
-	//½Ñ½º½Ã
-	_item[5].name = "Egg";
-	_item[5]._pluspunch = 1;
-	_item[5]._recoveryhp = 10;
-	_item[5]._plusmaxHP = 2;
-	_item[5]._price1 = 2;
-	_item[5]._price2 = 25;
-
-	_item[6].name = "octopus";
-	_item[6]._plusweapon = 1;
-	_item[6]._recoveryhp = 8;
-	_item[6]._plusmaxHP = 2;
-	_item[6]._price1 = 2;
-	_item[6]._price2 = 25;
-
-	_item[7].name = "squid";
-	_item[7]._pluspower = 2;
-	_item[7]._recoveryhp = 4;
-	_item[7]._plusmaxHP = 1;
-	_item[7]._price1 = 2;
-	_item[7]._price2 = 25;
-
-	_item[8].name = "conger Eel";
-	_item[8]._plusguard = 2;
-	_item[8]._recoveryhp = 16;
-	_item[8]._plusmaxHP = 4;
-	_item[8]._price1 = 3;
-	_item[8]._price2 = 75;
-
-	_item[9].name = "Prawn";
-	_item[9]._plusendure = 2;
-	_item[9]._recoveryhp = 32;
-	_item[9]._plusmaxHP = 8;
-	_item[9]._price1 = 6;
-
-	//ºñ½ÑÃÊ¹ä
-
-	_item[10].name = "Salmon";
-	_item[10]._pluspunch = 1;
-	_item[10]._plusenergy = 2;
-	_item[10]._recoveryhp = 40;
-	_item[10]._plusmaxHP = 10;
-	_item[10]._price1 = 8;
-	_item[10]._price2 = 25;
-
-	_item[11].name = "Ark Shell";
-	_item[11]._plusweapon = 1;
-	_item[11]._plusenergy = 2;
-	_item[11]._recoveryhp = 50;
-	_item[11]._plusmaxHP = 12;
-	_item[11]._price1 = 9;
-
-	_item[12].name = "Sea Urchin";
-	_item[12]._pluspower = 2;
-	_item[12]._plusenergy = 2;
-	_item[12]._recoveryhp = 64;
-	_item[12]._plusmaxHP = 16;
-	_item[12]._price1 = 11;
-	_item[12]._price2 = 50;
-
-	_item[13].name = "Halibut";
-	_item[13]._plusguard = 3;
-	_item[13]._plusenergy = 7;
-	_item[13]._recoveryhp = 80;
-	_item[13]._plusmaxHP = 20;
-	_item[13]._price1 = 18;
-	_item[13]._price2 = 50;
-
-	_item[14].name = "SwordFish";
-	_item[14]._pluspunch = 4;
-	_item[14]._pluspower = 4;
-	_item[14]._plusendure = 4;
-	_item[14]._plusenergy = 5;
-	_item[14]._recoveryhp = 80;
-	_item[14]._plusmaxHP = 20;
-	_item[14]._price1 = 28;
-	_item[14]._price2 = 75;
+	if(_name == "Coffee")
+	{
+		_plusenergy = 2;
+		_recoveryhp = 16;
+		_plusmaxHP = 4;
+		_price2 = 95;
+	}
+	if(_name == "Tea")
+	{ 
+		_plusenergy = 4;
+		_price2 = 95;
+	}
+	if (_name == "Hot Cocoa")
+	{
+		_plusguard = 1;
+		_recoveryhp = 24;
+		_plusmaxHP = 6;
+		_price1 = 1;
+		_price2 = 25;
+	}
+	if (_name == "Pancakes")
+	{
+		_recoveryhp = 32;
+		_plusmaxHP = 8;
+		_price1 = 3;
+		_price2 = 30;
+	}
 	
-	//ROLL
+	if(_name == "Waffles")
+	{
+		_plusagility = 1;
+		_plusendure = 1;
+		_recoveryhp = 32;
+		_plusmaxHP = 8;
+		_price1 = 4;
+		_price2 = 10;
+	}
+	//½ÑÃÊ¹ä
+	if (_name =="EGG" )
+	{
+		_pluspunch = 1;
+		_recoveryhp = 10;
+		_plusmaxHP = 2;
+		_price1 = 2;
+		_price2 = 25;
+	}
 
-	_item[15].name = "salad Roll";
-	_item[15]._recoveryhp = 20;
-	_item[15]._plusmaxHP = 5;
-	_item[15]._price1 = 2;
+	if (_name == "octopus")
+	{
+		_plusweapon = 1;
+		_recoveryhp = 8;
+		_plusmaxHP = 2;
+		_price1 = 2;
+		_price2 = 25;
+	}
+	if (_name == "squid")
+	{
+		_pluspower = 2;
+		_recoveryhp = 4;
+		_plusmaxHP = 1;
+		_price1 = 2;
+		_price2 = 25;
+	}
+	
+	if (_name == "conger Eel")
+	{
+		_plusguard = 2;
+		_recoveryhp = 16;
+		_plusmaxHP = 4;
+		_price1 = 3;
+		_price2 = 75;
+	}
 
-	_item[16].name = "TunaRoll";
-	_item[16]._recoveryhp = 32;
-	_item[16]._plusmaxHP = 8;
-	_item[16]._price1 = 3;
-	_item[16]._price2 = 75;
+	if (_name == "Prawn")
+	{
+		_plusendure = 2;
+		_recoveryhp = 32;
+		_plusmaxHP = 8;
+		_price1 = 6;
+	}
+	//ºñ½ÑÃÊ¹ä
+	if (_name == "Salmon")
+	{
+		_pluspunch = 1;
+		_plusenergy = 2;
+		_recoveryhp = 40;
+		_plusmaxHP = 10;
+		_price1 = 8;
+		_price2 = 25;
+	}
+	
+	if (_name == "Ark Shell")
+	{
+		_plusweapon = 1;
+		_plusenergy = 2;
+		_recoveryhp = 50;
+		_plusmaxHP = 12;
+		_price1 = 9;
+	}
+	if (_name == "Sea Urchin")
+	{
+		_pluspower = 2;
+		_plusenergy = 2;
+		_recoveryhp = 64;
+		_plusmaxHP = 16;
+		_price1 = 11;
+		_price2 = 50;
+	}
 
-	_item[17].name = "ShrimpRoll";
-	_item[17]._recoveryhp = 50;
-	_item[17]._plusmaxHP = 12;
-	_item[17]._price1 = 6;
-	_item[17]._price2 = 25;
+	if (_name =="Halibut")
+	{
+		_plusguard = 3;
+		_plusenergy = 7;
+		_recoveryhp = 80;
+		_plusmaxHP = 20;
+		_price1 = 18;
+		_price2 = 50;
+	}
 
-	_item[18].name = "MixedRoll";
-	_item[18]._recoveryhp = 80;
-	_item[18]._plusmaxHP = 20;
-	_item[18]._price1 = 10;
-	_item[18]._price2 = 50;
+	if (_name == "SwordFish")
+	{
+		_pluspunch = 4;
+		_pluspower = 4;
+		_plusendure = 4;
+		_plusenergy = 5;
+		_recoveryhp = 80;
+		_plusmaxHP = 20;
+		_price1 = 28;
+		_price2 = 75;
+	}
+	
+	//·Ñ
+	if (_name == "salad Roll")
+	{
+		_recoveryhp = 20;
+		_plusmaxHP = 5;
+		_price1 = 2;
+	}
+	
+	if (_name == "TunaRoll")
+	{
+		_recoveryhp = 32;
+		_plusmaxHP = 8;
+		_price1 = 3;
+		_price2 = 75;
+	}
+	
+	if (_name == "ShrimpRoll")
+	{
+		_recoveryhp = 50;
+		_plusmaxHP = 12;
+		_price1 = 6;
+		_price2 = 25;
+	}
+	
+	if (_name == "MixedRoll")
+	{
+		_recoveryhp = 80;
+		_plusmaxHP = 20;
+		_price1 = 10;
+		_price2 = 50;
+	}
 
+	//»§Áý
 
-	//ÈÞ´ë°¡´É
-	//shop2 »§Áý
-	_item[19].name = "Donut";
-	_item[19]._plusenergy = 6;
-	_item[19]._price2 = 80;
-	_item[19]._inventory = true;
+	if (_name == "Donut")
+	{
+		_plusenergy = 6;
+		_price2 = 80;
+		
+	}
 
-	_item[20].name = "Muffin";
-	_item[20]._plusagility = 1;
-	_item[20]._recoveryhp = 6;
-	_item[20]._plusmaxHP = 1;
-	_item[20]._price2 = 90;
-	_item[20]._inventory = true;
+	if (_name == "Muffin")
+	{
+		_plusagility = 1;
+		_recoveryhp = 6;
+		_plusmaxHP = 1;
+		_price2 = 90;
+	}
+	
+	if (_name == "Bagel")
+	{
+		_plusguard = 1;
+		_recoveryhp = 6;
+		_plusmaxHP = 1;
+		_price2 = 90;
+	}
 
-	_item[21].name = "Bagel";
-	_item[21]._plusguard = 1;
-	_item[21]._recoveryhp = 6;
-	_item[21]._plusmaxHP = 1;
-	_item[21]._price2 = 90;
-	_item[21]._inventory = true;
+	if (_name == "HoneyBun")
+	{
+		_plusendure = 1;
+		_recoveryhp = 6;
+		_plusmaxHP = 1;
+		_price2 = 90;
+	}
 
-	_item[22].name = "HoneyBun";
-	_item[22]._plusendure = 1;
-	_item[22]._recoveryhp = 6;
-	_item[22]._plusmaxHP = 1;
-	_item[22]._inventory = true;
-
-	_item[23].name = "Croissant";
-	_item[23]._recoveryhp = 8;
-	_item[23]._plusmaxHP = 2;
-	_item[23]._price1 = 1;
-	_item[23]._inventory = true;
+	if (_name == "Croissant")
+	{
+		_recoveryhp = 8;
+		_plusmaxHP = 2;
+		_price1 = 1;
+	}
 
 }
--- item.h
+++ item.h
@@ -1,35 +1,63 @@
 #pragma once
 #include "gameScene.h"
-#define IMAX 25 // 만들아이템 숫자
 
-struct itemstate
-{
-	string name;
-	int _price1;		//그냥달러1~10자리
-	int _price2;		//소수점
-	int _pluspunch;		//펀치	
-	int _pluskick;		//킥		
-	int _plusweapon;	//무기	
-	int _pluspower;		//힘	    
-	int _plusagility;	//민첩	
-	int _plusguard;		//방어	
-	int _plusendure;	//맷집	
-	int _plusenergy;	//기력	
-	int _recoveryhp;	//체력	
-	int _plusmaxHP;		//최대체력
-	bool _inventory;	// 인벤토리에 들어가는 탬인지 아닌지. 그냥 간단하게 불로 정의했어용 수정이 필요하시면 하시면됩니다아아아앙
-};
+//struct itemstate
+//{
+//	string name;
+//	int _price1;		//그냥달러1~10자리
+//	int _price2;		//소수점
+//	int _pluspunch;		//펀치	
+//	int _pluskick;		//킥		
+//	int _plusweapon;	//무기	
+//	int _pluspower;		//힘	    
+//	int _plusagility;	//민첩	
+//	int _plusguard;		//방어	
+//	int _plusendure;	//맷집	
+//	int _plusenergy;	//기력	
+//	int _recoveryhp;	//체력	
+//	int _plusmaxHP;		//최대체력
+//	int cnt;			//아이템 갯수
+//	bool _inventory;	// 인벤토리에 들어가는 탬인지 아닌지. 그냥 간단하게 불로 정의했어용 수정이 필요하시면 하시면됩니다아아아앙
+//};
 
 class item : public gameScene
 {
 private:
-
+string name;
+int _price1;		//그냥달러1~10자리
+int _price2;		//소수점
+int _pluspunch;		//펀치	
+int _pluskick;		//킥		
+int _plusweapon;	//무기	
+int _pluspower;		//힘	    
+int _plusagility;	//민첩	
+int _plusguard;		//방어	
+int _plusendure;	//맷집	
+int _plusenergy;	//기력	
+int _recoveryhp;	//체력	
+int _plusmaxHP;		//최대체력
+int cnt;			//아이템 갯수
 public:
-	itemstate _item[IMAX];
+	
 
-	HRESULT init() override;
+	HRESULT init(string _name);
 	void release() override;
 	void update() override;
 	void render() override;
-	void itemlist();
+	void itemlist(string _name);
+	
+	string getname() { return name; }
+	int getprice() { return _price1; }
+	int getprice2() { return _price2; }
+	int getpluspunch() { return _pluspunch; }
+	int getpluskick(){ return _pluskick; }
+	int getplusweapon() { return _plusweapon; }
+	int getpluspower() { return _pluspower; }
+	int getplusagility() { return _plusagility; }
+	int getplusguard() { return _plusguard; }
+	int getplusendure() { return _plusendure; }
+	int getplusenergy() { return _plusenergy; }
+	int getrecoveryhp() { return _recoveryhp; }
+	int getplusmaxHP() { return _plusmaxHP; }
+	
 };
